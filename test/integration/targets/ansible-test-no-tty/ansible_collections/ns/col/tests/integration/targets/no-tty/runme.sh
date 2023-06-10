a11a8e2f21a3d951c20500ab6a1f310b1e1e72aa
Matt Davis <mrd@redhat.com>
2023-06-09 17:39:18 -0700

    remove all non-docsite contents

--- test/integration/targets/ansible-test-no-tty/ansible_collections/ns/col/tests/integration/targets/no-tty/runme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-./assert-no-tty.py
--- test/integration/targets/old_style_vars_plugins/vars_plugins/require_enabled.py
+++
@@ -1,8 +0,0 @@
-from ansible.plugins.vars import BaseVarsPlugin
-
-
-class VarsModule(BaseVarsPlugin):
-    REQUIRES_ENABLED = True
-
-    def get_vars(self, loader, path, entities):
-        return {'require_enabled': True}
