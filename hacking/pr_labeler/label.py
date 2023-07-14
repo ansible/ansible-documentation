e79219a37f242c5136b98a5c95c8e87894284eca
Maxwell G <maxwell@gtmx.me>
2023-07-14 21:13:22 +0000

    labeler: refactor to use ctx obj; add triage label

--- hacking/pr_labeler/label.py
+++ hacking/pr_labeler/label.py
@@ -3,11 +3,15 @@
 
 from __future__ import annotations
 
+import dataclasses
 import os
+from collections.abc import Collection
+from functools import cache
 from pathlib import Path
 
 import github
 import github.Auth
+import github.Repository
 import github.PullRequest
 import typer
 from codeowners import CodeOwners, OwnerTuple
@@ -22,17 +26,45 @@ ROOT = HERE.parent.parent
 CODEOWNERS = (ROOT / ".github/CODEOWNERS").read_text("utf-8")
 
 
-def handle_codeowner_labels(pr: github.PullRequest.PullRequest) -> None:
+@dataclasses.dataclass(frozen=True)
+class LabelerCtx:
+    client: github.Github
+    repo: github.Repository.Repository
+    pr: github.PullRequest.PullRequest
+    dry_run: bool
+
+
+@cache
+def get_previously_labeled(ctx: LabelerCtx) -> frozenset[str]:
+    previously_labeled: set[str] = set()
+    for event in ctx.pr.get_issue_events():
+        if event.event in ("labeled", "unlabeled"):
+            assert event.label
+            previously_labeled.add(event.label.name)
+    return frozenset(previously_labeled)
+
+
+def handle_codeowner_labels(ctx: LabelerCtx) -> None:
     labels = LABELS_BY_CODEOWNER.copy()
     owners = CodeOwners(CODEOWNERS)
-    files = pr.get_files()
+    files = ctx.pr.get_files()
     for file in files:
         for owner in owners.of(file.filename):
             if labels_to_add := labels.pop(owner, None):
-                print("Adding labels to", f"{pr.id}:", *map(repr, labels_to_add))
-                pr.add_to_labels(*labels_to_add)
+                add_label_if_new(ctx, labels_to_add)
         if not labels:
             return
+
+
+def add_label_if_new(ctx: LabelerCtx, labels: Collection[str] | str) -> None:
+    """
+    Add a label to a PR if it wasn't added in the past
+    """
+    labels = {labels} if isinstance(labels, str) else labels
+    previously_labeled = get_previously_labeled(ctx)
+    print(f"Adding labels to {ctx.pr.number}:", *map(repr, labels))
+    if not ctx.dry_run:
+        ctx.pr.add_to_labels(*(set(labels) - previously_labeled))
 
 
 APP = typer.Typer()
@@ -46,14 +78,18 @@ def cb():
 
 
 @APP.command()
-def pr(pr_number: int) -> None:
-    gclient = github.Github(auth=github.Auth.Token(os.environ["GITHUB_TOKEN"]))
+def pr(pr_number: int, dry_run: bool = False) -> None:
+    gclient = github.Github(
+        auth=None if dry_run else github.Auth.Token(os.environ["GITHUB_TOKEN"]),
+    )
     repo = gclient.get_repo(f"{OWNER}/{REPO}")
     pr = repo.get_pull(pr_number)
     if pr.state != "open":
         print("Refusing to process closed ticket")
         return
-    handle_codeowner_labels(pr)
+    ctx = LabelerCtx(gclient, repo, pr, dry_run)
+    handle_codeowner_labels(ctx)
+    add_label_if_new(ctx, "needs_triage")
 
 
 if __name__ == "__main__":
