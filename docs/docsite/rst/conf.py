63d0772bca953ccf3b33c83d2e70902dc41dea1e
Sandra McCann <samccann@redhat.com>
2024-05-20 15:32:38 -0400

    update version switcher to suppor 2.17 (#1464) (#1487)
    (cherry picked from commit b737e3007cf76a8c98b77954d415cad529a59b5f)

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -239,7 +239,7 @@ html_context = {
     'current_version': version,
     'latest_version': (
         'devel' if tags.has('all') else
-        '2.16' if tags.has('core_lang') or tags.has('core') else
+        '2.17' if tags.has('core_lang') or tags.has('core') else
         '2.10' if tags.has('2.10') else
         '8' if tags.has('ansible')
         else '<UNKNOWN>'
@@ -248,7 +248,7 @@ html_context = {
     'available_versions': (
         ('devel',) if tags.has('all') else
         ('2.15_ja', '2.14_ja', '2.13_ja',) if tags.has('core_lang') else
-        ('2.16', '2.15', '2.14', 'devel',) if tags.has('core') else
+        ('2.17', '2.16', '2.15', 'devel',) if tags.has('core') else
         ('latest', '2.9', '2.9_ja', '2.8', 'devel') if tags.has('2.10') else
         ('latest', '2.9', 'devel') if tags.has('ansible')
         else '<UNKNOWN>'
