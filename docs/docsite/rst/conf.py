1122df3523a419ea36d480f45cbdf99eaf84537e
Sandra McCann <samccann@redhat.com>
2024-06-04 10:52:44 -0400

    update to version 10 (#1551)

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -46,7 +46,7 @@ VERSION = (
      # Controls branch version for core releases
     '2.17' if tags.has('core_lang') or tags.has('core') else
     # Controls branch version for Ansible package releases
-    'devel' if tags.has('ansible') or tags.has('all')
+    '10' if tags.has('ansible') or tags.has('all')
     else '<UNKNOWN>'
 )
 AUTHOR = 'Ansible, Inc'
