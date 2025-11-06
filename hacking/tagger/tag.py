96cbcfd4dd12a790c5122aa391555b63506bdb02
Don Naro <dnaro@redhat.com>
2025-11-06 08:00:23 +0000

    Update tagger and pip-compile workflow for active stable versions (#3216)
    * drop stable-2.17 from pip compile workflow
    
    * remove no longer active versions from tagger

--- hacking/tagger/tag.py
+++ hacking/tagger/tag.py
@@ -42,8 +42,6 @@ ROOT = HERE.parent.parent
 DEFAULT_ANSIBLE_CORE_CHECKOUT = ROOT.parent.joinpath("ansible")
 DEFAULT_REMOTE = "origin"
 DEFAULT_ACTIVE_BRANCHES: tuple[str, ...] = (
-    "stable-2.16",
-    "stable-2.17",
     "stable-2.18",
     "stable-2.19",
     "stable-2.20",
