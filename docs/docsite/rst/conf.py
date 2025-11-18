6498c21b96272037521baf8f037577e210b3466d
Sandra McCann <samccann@redhat.com>
2025-11-18 12:55:41 -0500

    update latest to 13

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -52,7 +52,7 @@ VERSION = (
      # Controls branch version for core releases
     '2.20' if tags.has('core_lang') or tags.has('core') else
     # Controls branch version for Ansible package releases
-    'devel' if tags.has('ansible') or tags.has('all')
+    '13' if tags.has('ansible') or tags.has('all')
     else '<UNKNOWN>'
 )
 AUTHOR = 'Ansible, Inc'
