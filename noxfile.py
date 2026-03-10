3f03793a5e3fd294a685537ac877d5f03615ab74
Don Naro <dnaro@redhat.com>
2026-03-10 18:56:30 +0000

    add py312 as target version for black formatting (#3517)
    (cherry picked from commit d924d1ab5faf4c95d470e55e859395582b5b09d4)

--- noxfile.py
+++ noxfile.py
@@ -83,7 +83,7 @@ def formatters(session: nox.Session):
     """
     install(session, req="formatters")
     session.run("isort", *session.posargs, *LINT_FILES)
-    session.run("black", *session.posargs, *LINT_FILES)
+    session.run("black", "--target-version", "py312", *session.posargs, *LINT_FILES)
 
 
 @nox.session
@@ -93,7 +93,9 @@ def formatters_check(session: nox.Session):
     """
     install(session, req="formatters")
     session.run("isort", "--check", *session.posargs, *LINT_FILES)
-    session.run("black", "--check", *session.posargs, *LINT_FILES)
+    session.run(
+        "black", "--check", "--target-version", "py312", *session.posargs, *LINT_FILES
+    )
 
 
 @nox.session
